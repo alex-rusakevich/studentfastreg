310df06099efb3c0c31bee956e5c6ebd95dba883
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-10-05 18:25:37 +0300

    #patch fixed windows build

--- build.ps1
+++ build.ps1
@@ -1,3 +1,9 @@
+# Set-ExecutionPolicy Bypass -Scope Process -Force; .\build.ps1
+
+function refreshenv {
+	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+}
+
 $command = "pyenv"
 
 if (Get-Command $command -ErrorAction SilentlyContinue) {
@@ -5,13 +11,17 @@ if (Get-Command $command -ErrorAction SilentlyContinue) {
 } else {
     Write-Host "$command does not exist in the shell"
     Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+	Remove-Item "./install-pyenv-win.ps1" -Force -Confirm:$false
 }
 
-pyenv install "3.8.20"
-pyenv shell "3.8.20"
+refreshenv
+
+pyenv install "3.8.10"
+pyenv shell "3.8.10"
 
 python -m pip install pipenv
-pipenv install
-pipenv install --dev
+python -m pipenv install
+python -m pipenv install --dev
 
-inv build
+pipenv run inv build
+explorer .\dist
