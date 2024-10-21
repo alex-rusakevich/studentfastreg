6dceba89af3e9c905dcd2ef5f62803999982645d
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-10-21 14:09:41 +0300

    #patch moved sfr to json

--- studentfastreg/serializers/plain.py
+++ studentfastreg/serializers/plain.py
@@ -1,12 +1,14 @@
+import base64
 import datetime
+import json
 import logging
-from io import TextIOWrapper
-from typing import Optional
+import os
+from io import BytesIO
+from typing import Optional, Union
 
-import py7zr
-import py7zr.exceptions
-import py7zr.py7zr
-import toml
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from PyQt6.QtWidgets import QDateEdit, QLineEdit, QRadioButton, QSpinBox
 
 import studentfastreg
@@ -28,15 +30,13 @@ class SFRPlainSerializer(SFRSerializer):
         meta_dict = {}
         meta_dict["format"] = self.FORMAT
         meta_dict["version"] = studentfastreg.__version__
-        meta_dict["timestamp"] = datetime.datetime.now()
+        meta_dict["timestamp"] = datetime.datetime.now().strftime("%d.%m.%Y, %H:%M")
         meta_dict["organization"] = ORGANIZATION
 
         if password is None:
             meta_dict["encrypted"] = False
         else:
             meta_dict["encrypted"] = True
-
-        meta_str = toml.dumps(meta_dict)
 
         data_dict = {}
 
@@ -47,34 +47,81 @@ class SFRPlainSerializer(SFRSerializer):
             ):
                 data_dict[widget.property("dataset_name")] = widget.text().strip()
             elif type(widget) is QDateEdit:
-                data_dict[widget.property("dataset_name")] = widget.date().toPyDate()
+                data_dict[widget.property("dataset_name")] = (
+                    widget.date().toPyDate().strftime("%d.%m.%Y")
+                )
             elif type(widget) is QRadioButton:
                 data_dict[widget.property("dataset_name")] = widget.isChecked()
             elif type(widget) is QSpinBox:
                 data_dict[widget.property("dataset_name")] = widget.value()
 
-        data_str = toml.dumps(data_dict)
+        file_dict = {"meta": meta_dict}
 
-        with py7zr.SevenZipFile(
-            file_out,
-            "w",
-            password=password,
-            header_encryption=True if password else False,
-        ) as archive:
-            archive.writestr(meta_str, "manifest.toml")
-            archive.writestr(data_str, "data.toml")
+        if password is None:
+            file_dict["data"] = data_dict
+        else:
+            salt = os.urandom(16)
+
+            kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=salt,
+                iterations=390000,
+            )
+            key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+
+            fernet = Fernet(key)
+            enc_data_bytes = fernet.encrypt(
+                json.dumps(
+                    data_dict, ensure_ascii=False, separators=(",", ":")
+                ).encode()
+            )
+
+            data = BytesIO()
+            data.write(salt)
+            data.write(enc_data_bytes)
+            data.seek(0)
+
+            file_dict["data"] = base64.b64encode(data.read()).decode("ascii")
+
+        with open(file_out, "w", encoding="utf8") as f:
+            json.dump(file_dict, f, ensure_ascii=False, separators=(",", ":"))
+
+    def _load_data(
+        self, file_in: str, password: Optional[str] = None
+    ) -> Union[dict, dict]:
+        with open(file_in, "r", encoding="utf8") as f:
+            file_dict = json.load(f)
+
+        meta_dict = file_dict["meta"]
+        data = file_dict["data"]
+
+        if meta_dict["encrypted"]:
+            data = base64.b64decode(data)
+
+            salt = data[:16]
+            data = data[16:]
+
+            kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=salt,
+                iterations=390000,
+            )
+            key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+
+            fernet = Fernet(key)
+
+            dec_data_bytes = fernet.decrypt(data).decode()
+
+            data_dict = json.loads(dec_data_bytes)
+        else:
+            data_dict = data
+
+        return (meta_dict, data_dict)
 
     def deserialize(self, file_in: str, password: Optional[str] = None) -> None:
-        with py7zr.SevenZipFile(
-            file_in,
-            "r",
-            password=password,
-            header_encryption=True if password else False,
-        ) as archive:
-            files = archive.readall()
-
-        meta_dict = toml.load(TextIOWrapper(files["manifest.toml"], encoding="utf8"))
-        data_dict = toml.load(TextIOWrapper(files["data.toml"], encoding="utf8"))
+        meta_dict, data_dict = self._load_data(file_in, password)
 
         # region Check meta
         if meta_dict["format"] != self.FORMAT:
@@ -108,7 +155,10 @@ class SFRPlainSerializer(SFRSerializer):
                 widget.setText(data_dict[k])
             elif type(widget) is QDateEdit:
                 widget: QDateEdit
-                widget.setDate(v)
+                v: str
+
+                datetime_from_val = datetime.datetime.strptime(v, "%d.%m.%Y")
+                widget.setDate(datetime_from_val)
             elif type(widget) is QRadioButton:
                 widget: QRadioButton
                 widget.setChecked(data_dict[k])
@@ -120,27 +170,15 @@ class SFRPlainSerializer(SFRSerializer):
                 return
 
     def does_file_has_password(self, file_in: str):
-        try:
-            with py7zr.SevenZipFile(
-                file_in,
-                "r",
-                password=None,
-            ) as archive:
-                archive.readall()
-        except py7zr.exceptions.PasswordRequired:
-            return True
-        else:
-            return False
+        return (
+            json.load(open(file_in, "r", encoding="utf8"))["meta"]["encrypted"] is True
+        )
 
     def is_password_correct(self, file_in: str, password: str):
         try:
-            with py7zr.SevenZipFile(
-                file_in,
-                "r",
-                password=password,
-            ) as archive:
-                archive.readall()
-        except Exception:
+            self._load_data(file_in, password)
+        except Exception as e:
+            logger.exception(e)
             return False
         else:
             return True
